912f21972a1579ac360573da2369b16e09dcd31f
jim2107054 <mdjahid205hasan@gmail.com>
2025-05-03 22:17:59 +0600

    .

--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import MainLayout from '../components/MainLayout'
+import { useNavigate } from 'react-router-dom'
 
 const LandingPage = () => {
+  const navigate = useNavigate();
   return (
     <MainLayout>
       <div className="bg-primaryText h-[90.5vh] grid grid-cols-[1fr_1fr] gap-24">
         <div className="flex flex-col justify-center gap-y-10 px-32">
           <h1 className='text-white text-6xl'>Health in Your<br/> Hands.</h1>
           <p className='text-white'>Take control of your healthcare with CareMate. Book appointments with ease, explore health blogs, and stay on top of your well-being, all in one place.</p>
-          <button className='login-btn w-1/3 hover:bg-hoverText h-14 text-3xl font-semibold hover:text-white'>Get Started</button>
+          <button onClick={()=>navigate('/login')} className='login-btn w-1/3 hover:bg-hoverText h-14 text-3xl font-semibold hover:text-white'>Get Started</button>
         </div>
         <div className="">
           <img src='/src/assets/header_img.png' className='w-4/5 mt-16'></img>
